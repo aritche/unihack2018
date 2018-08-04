7c72f82758f4b091c45ee548a4c69a47fc0fb0cd
Joshua Bennett <jbennett96@live.com>
2018-08-05 00:22:17 +1000

    Ammend the PreReq Function

--- searchPreReq.py
+++ searchPreReq.py
@@ -1,34 +1,4 @@
-class Course(object):
-    unitName = ""
-    preReq1 = ""
-    preReq2 = ""
 
-def makeNewCourse(unitName, preReq1 = "", preReq2 = ""):
-	course = Course
-	course.unitName = unitName
-	course.preReq1 = preReq1
-	course.preReq2 = preReq2
-	return course
-	
-
-# **with open("db.txt","r") as file:
-	# data = file.readlines()
-	# for line in data:
-
-		# lines = line.split(",")
-		# print lines
-		# for words in lines:
-			# words = words.replace("\n", "")
-		# length = len(words)
-		# if(length == 1):
-			# unit1 = makeNewCourse(words[0])
-		# elif(length == 2):
-			# unit2 = makeNewCourse(words[0],words[1])
-		# elif(length == 3):
-			# unit3 = makeNewCourse(words[0],words[1],words[2])
-
-			
-			
 def searchPreReq(currentCourses):
 	matches = []
 	with open("db.txt","r") as file:
@@ -49,5 +19,5 @@ def searchPreReq(currentCourses):
 						matches.append(courses)
 	return list(set(matches)) 
 					
-CCList = ["ACCT4444","BSBS2213"]
+CCList = ["ACCT4444","BSBS2213"] 
 print searchPreReq(CCList)
